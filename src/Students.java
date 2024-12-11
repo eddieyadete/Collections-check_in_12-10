b7efc42bf27796ac684e1c9a1e99613edcc04151
eddie yadete <eddieyadete123@gmail.com>
2024-12-11 08:51:05 -0600

    check-in collectionss

--- src/Students.java
+++ src/Students.java
@@ -4,8 +4,8 @@ import java.util.Collections;
 
 public class Students {
 
-    public static void main(String[] args) {
-        List<String> students = new ArrayList<>();
+    public void studentList(){
+        ArrayList<String> students = new ArrayList<>();
         students.add("jake todd");
         students.add("ane doe");
         students.add("leehee doe");
